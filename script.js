4984f7f05b6265b823e1ed9ab145ad993f333990
kevin diesenberg <kdiesenb@gmail.com>
2022-10-18 12:56:47 -0400

    adding an ul under hobbies

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const h2 = document.createElement("h2");
+/* const h2 = document.createElement("h2");
 h2.textContent = "this content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h2); */
+
+
+/*mouseover event to enlarge image when mouse clicks the img
+
+*/
+function addingEventListener() {
+    const lookingOut = document.getElementById("skydive");
+    lookingOut.addEventListener('click', function () {
+        alert("Looking towards the distant horizon");
+    });
+};
