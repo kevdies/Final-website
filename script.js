abc8bb9a5fb85290b74ea55cb8e313e063bd46bf
kevin diesenberg <kdiesenb@gmail.com>
2022-10-20 15:18:16 -0400

    fixed img alert, partial fix on buttons

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ document.querySelector("body").appendChild(h2); */
 
 /*mouseover event to enlarge image when mouse clicks the img
 */
-function addingEventListener() {
+
     const lookingOut = document.getElementById("skydive");
     lookingOut.addEventListener('click', function () {
         alert("Looking towards the horizon");
     });
-};
 
-let careerFocusButton = document.querySelector("button #careerFocus");
+
+let careerFocusButton = document.querySelector("button#careerFocus");
 let careerFocus = document.querySelector("div.careerFocus");
 careerFocus.style.display = "none";
 
 careerFocusButton.onclick = () => {
     careerFocus.style.display = "block";
+};
+
+careerFocusButton.dblclick = () => {
+    careerFocus.style.display = "none";
 };
